be44919125243c77441b5190e18d1a3f7f6e098d
mrengingeer <taninder22@live.ca>
2026-09-22 12:53:27 -0400

    updated build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mrengingeer.github.io/portfolio",
+  site: "https://mrengingeer.github.io",
   base: "/portfolio",
 
   integrations: [tailwind(), react()],
